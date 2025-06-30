b4e76b67b7342cf1b6e14331a05a6423435b9748
Gamal-Ehab <gamalehabg@gmail.com>
2025-07-01 00:22:39 +0300

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -11,6 +11,19 @@
             //Console.WriteLine(num);
             #endregion
 
+            #region Q2
+            ////Q2
+
+            //Console.WriteLine("Enter String: ");
+            //int value = Convert.ToInt32(Console.ReadLine());
+            ////int value = int.Parse(Console.ReadLine());
+            //////int.TryParse(Console.ReadLine(), out int value);
+            //Console.WriteLine(value);
+
+            //What Will Happen?
+            //1. Using Convert & Parse : It will throw exeption.
+            //2. Using Try Parse : Will Print 0
+            #endregion
         }
     }
 }
