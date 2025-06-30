d86448dff3d407b3575f68f094b3c02ccba363c3
Gamal-Ehab <gamalehabg@gmail.com>
2025-06-30 20:50:17 +0300

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Fractions & Discards
+            //Fractions===========================================
+            //float X = 3.55859468874548f;
+            //double Y = 3.5;
+            //decimal Z = 3.5m;
+            //Console.WriteLine(X);
+
+            //var W = 3.5f;
+
+            
+            //Discards============================================
+            //long L = 351_572_964_875;
+            //Console.WriteLine($"{L:c}");
+
+            //var cultureInfo = new CultureInfo("ar-SA");
+            //Console.WriteLine(L.ToString("C", cultureInfo));
+            #endregion
+        }
+    }
+}
